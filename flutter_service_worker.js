70db922ad142c99a0e5cbb6fb317d0c797ef4c1c
samuel2202-anis <65362048+samuel2202-anis@users.noreply.github.com>
2022-06-15 00:02:48 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c2ce19c5c7a9858c74426625ea79a361",
-"/": "c2ce19c5c7a9858c74426625ea79a361",
-"main.dart.js": "c427f412f77446fc04ea87f7c7412dc5",
+"index.html": "365b3c5e5d75f267d431e45832c8d426",
+"/": "365b3c5e5d75f267d431e45832c8d426",
+"main.dart.js": "c4064039fb3aab6396dc3e3cd3cea3cd",
 "manifest.json": "78ae7ad75bf829c88ed8e4f105b734d3",
 "version.json": "547c428716033683c36754dba8d8adb6"
 };
